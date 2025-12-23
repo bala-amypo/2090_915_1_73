fcca6f52d453acad5c84acabf34e9b085e87bb63
bala-amypo <bala@amypo.in>
2025-12-23 06:33:58 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConstants.java
+++ src/main/java/com/example/demo/security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.example.demo.security;
 
-public class SecurityConstants {
+public class SecurityConstants{
     public static final String SECRET_KEY = "demo-secret";
     public static final long EXPIRATION_TIME = 86400000;
 }
