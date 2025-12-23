00eea09d5d8605f6e1c847e71461043a1934aeb2
bala-amypo <bala@amypo.in>
2025-12-23 06:27:56 +0000

    User commit

--- src/main/java/com/example/demo/exception/UnathorizedException.java
+++ src/main/java/com/example/demo/exception/UnathorizedException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String msg) {
+public class UnathorizedException extends RuntimeException {
+    public UnathorizedException(String msg) {
         super(msg);
     }
 }
--- src/main/java/com/example/demo/security/JwtAuthentionFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthentionFilter.java
@@ -9,7 +9,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthentionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
